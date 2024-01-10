9e2a590030d248db648f1130a3ba1dc18198eb32
nguyenthanguth <82599819+nguyenthanguth@users.noreply.github.com>
2024-01-10 19:29:40 +0700

    240110

---
+++ 04.AnalysPoint/Program.cs
@@ -0,0 +1,42 @@
+﻿using _04_AnalysPoint;
+using System;
+
+namespace _04.AnalysPoint
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+
+            //Load sample data
+            var sampleData = new MLModel.ModelInput()
+            {
+                Col0 = 3501.964F,
+                Col1 = 2743.525F,
+            };
+
+            //Load model and predict output
+            MLModel.ModelOutput result = MLModel.Predict(sampleData);
+            Console.WriteLine(result.Col0);
+            Console.WriteLine(result.Col1);
+            Console.WriteLine(result.Col2);
+            Console.WriteLine(result.PredictedLabel);
+            Console.WriteLine(string.Join(" ", result.Features));
+            Console.WriteLine(string.Join(" ", result.Score));
+        }
+
+        public static string Analys(float X, float Y)
+        {
+            MLModel.ModelInput sampleData = new MLModel.ModelInput()
+            {
+                Col0 = X,
+                Col1 = Y,
+            };
+
+            MLModel.ModelOutput predict = MLModel.Predict(sampleData);
+            string show = $"{predict.PredictedLabel}\r\n{string.Join(" ", predict.Features)}\r\n{string.Join(" ", predict.Features)}";
+            return show;
+        }
+    }
+}
---
+++ PredictPoint/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+using Microsoft.ML.Data;
+using static PredictPoint.MLModel;
+using Application = Autodesk.AutoCAD.ApplicationServices.Application;
+
+namespace PredictPoint
+{
+    public class Class1
+    {
+        [CommandMethod("Hello")]
+        public static void Hello()
+        {
+            Application.DocumentManager.MdiActiveDocument.Editor.WriteMessage("hello world net framework 4.8");
+        }
+
+        [CommandMethod("Predict")]
+        public static void Predict()
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Database db = doc.Database;
+            Editor ed = doc.Editor;
+
+            while (true)
+            {
+                PromptPointResult ppr = ed.GetPoint("Pick");
+                if (ppr.Status != PromptStatus.OK)
+                {
+                    return;
+                }
+                Point3d point = ppr.Value;
+
+                ModelInput modelInput = new ModelInput();
+                modelInput.Col0 = Convert.ToSingle(point.X);
+                modelInput.Col1 = Convert.ToSingle(point.Y);
+
+                ModelOutput modelOutput = MLModel.Predict(modelInput);
+                for (int i = 0; i < modelOutput.Score.Length; i++)
+                {
+                    modelOutput.Score[i] = Convert.ToInt32(modelOutput.Score[i] * 100);
+                }
+
+                MessageBox.Show($"Nhóm: {modelOutput.PredictedLabel}\r\n" +
+                    $"Score: {string.Join("%\t", modelOutput.Score)}\r\n" +
+                    $"Distance: {string.Join("mm\t", modelOutput.Features)}");
+
+            }
+        }
+    }
+}
